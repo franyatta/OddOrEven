fec30da063c1e73a88a90575a5c7ea136b064602
franyatta <95552724+franyatta@users.noreply.github.com>
2023-09-16 16:09:57 -0400

    Add files via upload

---
+++ OddOrEven.java
@@ -0,0 +1,18 @@
+// learning about if else statements in class currently
+// try not to go above the overflow number
+import java.util.Scanner;
+public class OddOrEven {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int num = input.nextInt();
+		
+		if (num % 2 == 0) 
+		System.out.println("Your number is an even number");
+		else
+			System.out.println("Your number is and odd number");
+	}
+
+}
+// franyatta
